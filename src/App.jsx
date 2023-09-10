8f1546c70932629e353416c662265de523831cdf
ahmed.bachir <ahmed.bachir@emploipartner.com>
2023-09-10 17:03:29 +0100

    selectors

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,48 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
-import OrderItem from "./features/order/OrderItem";
-import AppLayout from "./ui/AppLayout";
-import { getMenu, getOrder } from "./services/apiRestaurant";
-import "./index.css";
-import Error from "./ui/Error";
-import Order from "./features/order/Order";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Home from './ui/Home';
+import Error from './ui/Error';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder, {
+  action as createOrderAction,
+} from './features/order/CreateOrder';
+import Order, { loader as orderLoader } from './features/order/Order';
+import AppLayout from './ui/AppLayout';
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      { path: '/cart', element: <Cart /> },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  const router = createBrowserRouter([
-    {
-      element: <AppLayout />,
-      errorElement: <Error />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/menu",
-          element: <Menu />,
-          loader: async () => {
-            return await getMenu();
-          },
-          errorElement: <Error />,
-        },
-        {
-          path: "/cart",
-          element: <Cart />,
-        },
-        {
-          path: "/order/new",
-          element: <CreateOrder />,
-        },
-        {
-          path: "/order/:orderId",
-          element: <Order />,
-          loader: async ({ params }) => {
-            const { orderId } = params;
-            return await getOrder(orderId);
-          },
-          errorElement: <Error />,
-        },
-      ],
-    },
-  ]);
   return <RouterProvider router={router} />;
 }
 
